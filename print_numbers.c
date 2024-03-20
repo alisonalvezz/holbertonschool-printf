6f0812a151e2d153a4451ac49f235d447cb54821
Alison <8661@holbertonstudents.com>
2024-03-20 17:25:38 +0000

    commit

--- print_numbers.c
+++ print_numbers.c
@@ -8,13 +8,19 @@ int print_numbers(long int n)
 
 	if (n < 0)
 	{
-		write(1, "-", 1);
+		write(1, "-", 1); /**imprime el signo -*/
 		count++;
-		n = -n;
+		n = -n; /** convierte el numero a su valor absoluto*/
 	}
 
-	if (n / 10 != 0)
-		count = count + print_numbers(n / 10);
+	if (n / 10 == 0)
+	{
+		digit = n % 10 + '0';
+		write(1, &digit, 1);
+		return (1);
+	}
+
+	count =  print_numbers(n / 10);
 
 	digit = n % 10 + '0';
 	write(1, &digit, 1);
