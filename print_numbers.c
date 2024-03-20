d9d88e8df443fb487c4d1ac16068edffd70a11d6
Alison <8661@holbertonstudents.com>
2024-03-20 13:05:52 +0000

    commit

--- print_numbers.c
+++ print_numbers.c
@@ -1,3 +1,24 @@
 #include "main.h"
 
+int print_numbers(long int n)
+{
+	int count = 0;
+	
+	char digit;
 
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		count++;
+		n = -n
+	}
+
+	if (n / 10 != 0)
+		count = count + print_numbers(n / 10);
+
+	digit = n % 10 + '0';
+	write(1, &digit, 1);
+	count++;
+
+	return (count);
+}
